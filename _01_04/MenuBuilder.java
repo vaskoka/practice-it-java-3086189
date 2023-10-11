5e122a49c49008bd06a59239bd3f8dbcab32a34b
vaskoka <74137550+vaskoka@users.noreply.github.com>
2023-10-11 10:29:17 +0000

    Primitives and Clases done

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -7,26 +7,28 @@ public class MenuBuilder {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
-
+String menuTitle = "My Dream Menu:";
     // Print the menuTitle variable to the console.
-
+System.out.println(menuTitle);
     // Create a variable called menu of type ArrayList.
-
+ArrayList<MenuItem> menu = new ArrayList<>();
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
-
+MenuItem starter = new MenuItem("Greek salad");
     // Add the starter variable to the ArrayList called menu.
-
+menu.add(starter);
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
+    MenuItem mainCourse = new MenuItem("Salmon");
 
     // Add the mainCourse variable to the ArrayList called menu.
-
+menu.add(mainCourse);
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
-
+MenuItem dessert = new MenuItem("Turamisu");
     // Add the dessert variable to the ArrayList called menu.
-
+menu.add(dessert);
     // Print the menu variable to the console.
+    System.out.println(menu);
   }
 }
--- _02_03e/Ticket.java
+++ _02_03e/Ticket.java
@@ -10,4 +10,28 @@ public class Ticket {
   private double price;
   private boolean isReturn;
 
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean isReturn() {
+    return isReturn;
+  }
+
 }
